e375223be6be1daec3d057e6b7c5b5445ccd4272
DJ027972 <dwq940721>
2021-01-12 14:17:05 +0800

    springboot-learn

--- src/main/java/com/dong/AppStart.java
+++ src/main/java/com/dong/AppStart.java
@@ -8,9 +8,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 
+/**
+ * @SpringBootApplication 注解 包含@ComponentScan注解
+ * 我们可以通过basePackages等属性来细粒度的定制@ComponentScan自动扫描的范围，
+ * 如果不指定，则默认Spring框架实现会从声明@ComponentScan所在类的package进行扫描。
+ *
+ * 注：所以SpringBoot的启动类最好是放在root package下，因为默认不指定basePackages。
+ *
+ */
+
 @Log
 @MapperScan("com.dong")
-@SpringBootApplication(scanBasePackages = "com.dong")
+//@SpringBootApplication(scanBasePackages = "com.dong")
+@SpringBootApplication
 public class AppStart {
     public static void main(String[] args) {
         log.info("打印日志开始!!!");
--- src/main/java/com/dong/configuration/BeanConfiguration.java
+++ src/main/java/com/dong/configuration/BeanConfiguration.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @author dongwq  通过配置类生成对象
+ * @Configuration的注解类标识这个类可以使用Spring IoC容器作为bean定义的来源
+ * @Bean注解告诉Spring，一个带有@Bean的注解方法将返回一个对象，该对象应该被注册为在Spring应用程序上下文中的bean
  */
 @Configuration
 public class BeanConfiguration {
 
+    /**
+     * 创建的bean对象的beanid默认为方法名
+     * @return
+     */
    @Bean
-   public  UserDemo  generateUser(){
+   public  UserDemo  userDemo(){
        System.out.println("bean configuration start");
        return new UserDemo();
    }
