6a5071c0f23ea3f4fc9255928bc734923e276771
DJ027972 <dwq940721>
2021-01-12 17:07:49 +0800

    springboot-自动配置源码分析

--- src/main/java/com/dong/AppStart.java
+++ src/main/java/com/dong/AppStart.java
@@ -27,6 +27,30 @@ import org.springframework.context.annotation.ComponentScan;
  *  @EnableAutoConfiguration会根据类路径中的jar依赖为项目进行自动配置，如：添加了spring-boot-starter-web依赖，会自动添加Tomcat和Spring MVC的依赖，Spring Boot会对Tomcat和Spring MVC进行自动配置。
  *
  *
+ *
+ * @EnableAutoConfiguration 注解内容如下：
+ *
+ *      @Target({ElementType.TYPE})
+ *      @Retention(RetentionPolicy.RUNTIME)
+ *      @Documented
+ *      @Inherited
+ *      @AutoConfigurationPackage
+ *      @Import({AutoConfigurationImportSelector.class})
+ *
+ *      AutoConfigurationImportSelector.class 最为重要
+ *
+ *      借助AutoConfigurationImportSelector，@EnableAutoConfiguration可以帮助SpringBoot应用将所有符合条件的@Configuration配置都加载到当前SpringBoot创建并使用的IoC容器。
+ *
+ *      借助于Spring框架原有的一个工具类：SpringFactoriesLoader的支持，@EnableAutoConfiguration可以智能的自动配置功效才得以大功告成！
+ *
+ *      AutoConfigurationImportSelector里调用 SpringFactoriesLoader的示例代码如下：
+ *
+ *             通过AutoConfigurationImportSelector类的selectImports()方法,该方法中会调用如下示例的方法
+ *
+ *         protected List<AutoConfigurationImportFilter> getAutoConfigurationImportFilters() {
+ *              return SpringFactoriesLoader.loadFactories(AutoConfigurationImportFilter.class, this.beanClassLoader);
+ *         }
+ *
  */
 
 @Log
