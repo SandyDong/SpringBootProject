ec88e37d32e97f5e7405fe5380a062d6fa32155a
DJ027972 <dwq940721>
2021-02-09 10:49:07 +0800

    解析excel文件测试

---
+++ src/main/java/com/dong/controller/Admin/EasyExcelTest.java
@@ -0,0 +1,14 @@
+package com.dong.controller.Admin;
+
+import com.alibaba.excel.EasyExcel;
+
+/**
+ * @author dongwq
+ * @date 2021.02.09
+ * @description easyExcel解析excel测试
+ */
+public class EasyExcelTest {
+    public static void main(String[] args) {
+        EasyExcel.read();
+    }
+}
